c71e93212a238f4310aca7b671d79bcd5805e40f
ygowill <qq554552354@126.com>
2018-01-27 14:00:08 +0800

    edit 1.1 part

--- C1/1.1.cpp
+++ C1/1.1.cpp
@@ -1,16 +1,22 @@
 /*
 #include <iostream>
-#include <string>
+//#include <string>
 
 using namespace std;
 
 int main(){
-    string user_name;
+    string user_first_name;
+    string user_last_name;
     cout << "Please enter your first name: ";
-    cin >> user_name;
+    cin >> user_first_name;
+    cout << "Please enter your last name: ";
+    cin >> user_last_name;
     cout << '\n'
          << "Hello, "
-         << user_name
+         << user_first_name
+         << " "
+         << user_last_name
          << "...and goodbye!\n";
     return 0;
-}*/
+}
+*/
