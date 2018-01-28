c997c1d642bfff7101d8a8bac97f86c1560f5113
ygowill <qq554552354@126.com>
2018-01-28 15:40:18 +0800

    finish exercise 1.5

---
+++ C1/Exercise1.5.cpp
@@ -0,0 +1,32 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char nameCstyle[100];
+    string nameString;
+    while(true){
+        cout << "(C-style)请输入您的名字：";
+        cin >> nameCstyle;
+        if(nameCstyle[2]==0){
+            cout << "请重新输入！\n";
+            continue;
+        }
+        cout << "你好, " << nameCstyle;
+        break;
+    }
+    cout << "\n";
+
+    while(true){
+        cout << "(string)请输入您的名字：";
+        cin >> nameString;
+        if(nameString.size()<3){
+            cout << "请重新输入！\n";
+            continue;
+        }
+        cout << "你好, " << nameString;
+        break;
+    }
+    return 0;
+}*/
