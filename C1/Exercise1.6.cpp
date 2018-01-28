9feca983f32cdf93acac6ae54503709bcf54954e
ygowill <qq554552354@126.com>
2018-01-28 21:32:54 +0800

    finish exercise 1.6

---
+++ C1/Exercise1.6.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define MAXSIZE 100
+
+bool AllIsNum(string input){
+    for(int i=0;i<input.size();i++){
+        int tmp=(int)input[i];
+        if(tmp >='0' && tmp <='9'){
+            continue;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    string user_inpput;
+    int pos=0;
+    int theArray[MAXSIZE];
+    int num=0,sum=0;
+    double avg=0;
+    cout << "请输入整数: ";
+    cin >> user_inpput;
+    while(true){
+        if(!AllIsNum(user_inpput)){
+            break;
+        }
+        else{
+            theArray[pos++]=atoi(user_inpput.c_str());
+            num++;
+            cout << "请继续输入整数：";
+            cin >> user_inpput;
+        }
+    }
+
+    vector<int> theVector(theArray,theArray+MAXSIZE);
+    for(int i=0;i<num;i++){
+        sum+=theVector[i];
+    }
+    avg=(double)sum/num;
+    cout << "总和为：" << sum << "平均数为：" << avg;
+    return 0;
+}
